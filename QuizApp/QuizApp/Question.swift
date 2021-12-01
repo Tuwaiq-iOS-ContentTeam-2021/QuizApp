0f8eb54e25cda467790d54869702799d7cbd1b50
= <=>
2021-12-01 22:04:51 +0300

    Quiz App project

---
+++ QuizApp/QuizApp/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Rayan Taj on 01/12/2021.
+//
+
+import Foundation
+
+struct Question {
+    
+    var questionStatment : String
+    var answer : String
+    
+}
